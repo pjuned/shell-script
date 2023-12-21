8ceaa0ac541998cb5947e8725371acd08f5e1e52
junaid <pjuned.it@gmail.com>
2023-12-21 23:48:32 +0530

    install package

--- installpackages.sh
+++ installpackages.sh
@@ -4,8 +4,23 @@ ID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
-Y="\e[0m"
+Y="\e[33m"
+N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing t $TIMESTAMP" &>>  $LOGFILE
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+        echo -e "$2 ....$R Failed $N"
+    else 
+        echo -e "$2 ....$G Success $N"
+
+
+}
 
 if [ $ID -ne 0 ]
 then
@@ -15,4 +30,16 @@ else
     echo "you are root user"
 fi
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+
+for package in $@
+do 
+    yum list installed $package &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $?  "installation of $package"
+    else 
+        echo "$package is already installed..$Y hence skipping $N"
+    fi
+done
