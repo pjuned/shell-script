68c121ff619b63b1e720bb249ee9a8faad29c11a
junaid <pjuned.it@gmail.com>
2023-12-21 14:28:49 +0530

    adding variables.sh file

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "demo on varibales"
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1  : Hello $PERSON2, Good Morning"
+echo "$PERSON2 : Hello $PERSON1 I am fine, How are you $PERSON1"
+echo "$PERSON1 : I am fine $PERSON2"
