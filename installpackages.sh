5ad69ccde5fe85545f5d96b29f446cffa913b2b9
junaid <pjuned.it@gmail.com>
2023-12-21 23:16:11 +0530

    edited variables.sh

---
+++ installpackages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[0m"
+
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR :: Please run this script with root access $N"
+    exit 1 
+else
+    echo "you are root user"
+fi
+
+echo "All arguments passed: $@"
