c18a375f1485296c5764a210922dadd27e5af2dd
junaid <pjuned.it@gmail.com>
2023-12-21 15:02:54 +0530

    edited variables.sh

--- variables.sh
+++ variables.sh
@@ -8,3 +8,7 @@ PERSON2=Rahim
 echo "$PERSON1  : Hello $PERSON2, Good Morning"
 echo "$PERSON2 : Hello $PERSON1 I am fine, How are you $PERSON1"
 echo "$PERSON1 : I am fine $PERSON2"
+
+DATE=$(date)
+
+echo "date & time is $(DATE)"
