55688447c15df14d3b69801b09dea9bf61cc1d5b
junaid <pjuned.it@gmail.com>
2024-03-30 09:48:54 +0530

    2nd script added

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 person1=Ramesh
 person2=Suresh
 
-echo '$person1 : Hi $person2 Good morning'
-echo '$person2 : HI $person1 very good morning'
-echo '$person1: How are you $person2'
-echo '$person2: I am fine $person1, how are you'
+echo "$person1 : Hi $person2 Good morning"
+echo "$person2 : HI $person1 very good morning"
+echo "$person1: How are you $person2"
+echo "$person2: I am fine $person1, how are you"
