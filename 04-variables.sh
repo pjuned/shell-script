165ac87dbba20f71fd727fd5b6099aead217a683
junaid <pjuned.it@gmail.com>
2024-03-30 11:51:43 +0530

    command arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 : Hi $person2 Good morning"
+echo "$person2 : HI $person1 very good morning"
+echo "$person1: How are you $person2"
+echo "$person2: I am fine $person1, how are you"
