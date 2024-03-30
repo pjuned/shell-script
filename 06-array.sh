11e24dd2ffeea342f1a1bff4a8aef231621b9ced
junaid <pjuned.it@gmail.com>
2024-03-30 12:50:38 +0530

    command arguments

---
+++ 06-array.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "first value is : ${FRUITS[0]}"
+
+echo "second value is : ${FRUITS[1]}"
+
+echo "third value is : ${FRUITS[2]}"
+
+
+
+
+
+
