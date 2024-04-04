7effb1b8e52fd28b3e4c833a162bdab1cefad9fb
junaid <pjuned.it@gmail.com>
2024-04-04 16:43:25 +0530

    adding conditions

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-NUMBER=$1
+ID=$(id -u)
 
-if [ $NUMBER -gt 100 ]
-then
-    echo "given number $NUMBER is more than 100"
+if [ $(ID) -ne 0 ]
+then 
+    echo "you are not root user : please run this with Root user"
+    exit 1
 else 
-    echo "given number $NUMBER is less than 100"
+    echo "you are root user "
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR : mysql installation is failed"
+else
+    echo "mysql installation is successful"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR: git installation failed"
+else
+    echo "git installation is successful"
 fi
