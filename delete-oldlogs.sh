c27309f9184e743dc977f6df1c368a1442c6f220
junaid <pjuned.it@gmail.com>
2024-04-21 12:56:03 +0530

    delete fiel

---
+++ delete-oldlogs.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+SOURCE_DIR="/tmp/shellscript-logs"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ]
+then
+echo -e "$R Source Directory $SOURCE_DIR does not exist $N"
+fi
+
+FILES-TO_DELETE=$(find $SOURCE_DIR -type f +mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting file : $line"
+    rm -rf $line
+done <<< $FILES_TO_DELETE
+
+    
