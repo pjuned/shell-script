2eae3d2eedefd0c9aff9b17eca4ff0da35ceebfe
junaid <pjuned.it@gmail.com>
2024-04-21 13:57:34 +0530

    delete fiel

--- delete-oldlogs.sh
+++ delete-oldlogs.sh
@@ -16,7 +16,7 @@ echo -e "$R Source Directory $SOURCE_DIR does not exist $N"
 exit 1
 fi
 
-FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f +mtime +14 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
